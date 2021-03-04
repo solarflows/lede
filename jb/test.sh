8f77bc27190ead641e0bac28ab2c462d60bcb7bb
hyy-666 <1556047610@qq.com>
2021-03-04 14:47:31 +0800

    delete no needed files.

--- jb/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-s_dir=$(cd "$(dirname "$0")";pwd)
-work_dir=`pwd`/package/mydiy
-echo $work_dir/update.sh
--- jb/x86.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo -e "\033[32m Strat $0 \033[0m"
-s_dir=$(cd "$(dirname "$0")";pwd)
-$s_dir/start.sh
-$s_dir/compile.sh
