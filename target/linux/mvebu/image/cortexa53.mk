ae3473c414c8f9b38c2a355691d51662dfb1745d
hyy-666 <57535444+hyy-666@users.noreply.github.com>
2021-09-01 20:35:44 +0800

    catdrive:add support for sysupgrade image

--- target/linux/mvebu/image/cortexa53.mk
+++ target/linux/mvebu/image/cortexa53.mk
@@ -3,6 +3,8 @@ define Device/catdrive_catdrive
   DEVICE_VENDOR := CATDRIVE
   DEVICE_MODEL := CatDrive
   SOC := armada-3720
+  IMAGES += sysupgrade.bin
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += catdrive_catdrive
 
