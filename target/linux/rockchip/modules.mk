064b8fe507c0c408ef54d84533f1b5602fdd644d
Husky <1556047610@qq.com>
2022-08-27 20:03:09 +0800

    merge from immortalwrt

--- target/linux/rockchip/modules.mk
+++ target/linux/rockchip/modules.mk
@@ -17,7 +17,7 @@ define KernelPackage/drm-rockchip
 	CONFIG_DRM_DW_HDMI \
 	CONFIG_DRM_DW_HDMI_CEC \
 	CONFIG_ROCKCHIP_VOP=y \
-	CONFIG_ROCKCHIP_VOP2=y
+	CONFIG_ROCKCHIP_VOP2=y \
 	CONFIG_ROCKCHIP_ANALOGIX_DP=n \
 	CONFIG_ROCKCHIP_CDN_DP=n \
 	CONFIG_ROCKCHIP_DW_HDMI=y \
@@ -26,7 +26,7 @@ define KernelPackage/drm-rockchip
 	CONFIG_ROCKCHIP_LVDS=y \
 	CONFIG_ROCKCHIP_RGB=n \
 	CONFIG_ROCKCHIP_RK3066_HDMI=n \
-	CONFIG_DRM_DP_AUX_BUS@ge5.15 \
+	CONFIG_DRM_DP_AUX_BUS \
 	CONFIG_DRM_PANEL=y \
 	CONFIG_DRM_PANEL_BRIDGE=y \
 	CONFIG_DRM_PANEL_SIMPLE
@@ -34,10 +34,9 @@ define KernelPackage/drm-rockchip
 	$(LINUX_DIR)/drivers/gpu/drm/bridge/synopsys/dw-hdmi.ko \
 	$(LINUX_DIR)/drivers/gpu/drm/bridge/synopsys/dw-hdmi-cec.ko \
 	$(LINUX_DIR)/drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.ko \
-	$(LINUX_DIR)/drivers/media/cec/cec.ko@le5.4 \
-	$(LINUX_DIR)/drivers/media/cec/core/cec.ko@ge5.15 \
+	$(LINUX_DIR)/drivers/media/cec/core/cec.ko \
 	$(LINUX_DIR)/drivers/phy/rockchip/phy-rockchip-inno-hdmi.ko \
-	$(LINUX_DIR)/drivers/gpu/drm/drm_dp_aux_bus.ko@eq5.15 \
+	$(LINUX_DIR)/drivers/gpu/drm/drm_dp_aux_bus.ko \
 	$(LINUX_DIR)/drivers/gpu/drm/panel/panel-simple.ko \
 	$(LINUX_DIR)/drivers/gpu/drm/rockchip/rockchipdrm.ko
   AUTOLOAD:=$(call AutoProbe,rockchipdrm phy-rockchip-inno-hdmi dw-hdmi-cec)
@@ -69,3 +68,21 @@ define KernelPackage/gpu-lima/description
 endef
 
 $(eval $(call KernelPackage,gpu-lima))
+
+define KernelPackage/saradc-rockchip
+  SUBMENU:=$(IIO_MENU)
+  TITLE:=Rockchip SARADC support
+  DEPENDS:=@TARGET_rockchip +kmod-industrialio-triggered-buffer
+  KCONFIG:= \
+	CONFIG_RESET_CONTROLLER=y \
+	CONFIG_ROCKCHIP_SARADC
+  FILES:= \
+	$(LINUX_DIR)/drivers/iio/adc/rockchip_saradc.ko
+  AUTOLOAD:=$(call AutoProbe,rockchip_saradc)
+endef
+
+define KernelPackage/saradc-rockchip/description
+  Support for the SARADC found in SoCs from Rockchip
+endef
+
+$(eval $(call KernelPackage,saradc-rockchip))
