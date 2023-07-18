51509163d710ddc46c6c161f65da1f332dc2b945
Husky <1556047610@qq.com>
2023-07-18 16:08:47 +0800

    rockchip:try to fix rk7039ca not boot.
    修改了rockchip,pwm_voltage 900000.
    修改了boot-script去启用ttl输出.
    
    Update rk3399-rk7039ca.dts
    
    Update rk3399-rk7039ca.dts

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -347,7 +347,7 @@ define Device/cosofteck_rk7039ca
   DEVICE_MODEL := RK7039CA
   SOC := rk3399
   UBOOT_DEVICE_NAME := cosofteck-rk7039ca-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-bin | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-bin | gzip | append-metadata
   DEVICE_PACKAGES := kmod-r8168 -urngd kmod-igb-rockchip kmod-brcmfmac cypress-firmware-4356-sdio wpad-openssl scensmart-sv901-firmware
 endef
 TARGET_DEVICES += cosofteck_rk7039ca
