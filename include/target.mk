a0588edf6ced66c15501c42bc91efb1e827aaa97
hyy-666 <hyiy666@gmail.com>
2023-12-31 15:18:15 +0800

    remove some packages.

--- include/target.mk
+++ include/target.mk
@@ -56,9 +56,8 @@ DEFAULT_PACKAGES.router:=\
 	dnsmasq-full firewall iptables ppp ppp-mod-pppoe \
 	block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun \
 	iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings luci luci-newapi \
-	ddns-scripts_aliyun ddns-scripts_dnspod luci-app-ddns luci-app-upnp luci-app-autoreboot \
-	luci-app-arpbind luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus luci-app-vlmcsd \
-	luci-app-accesscontrol luci-app-nlbwmon luci-app-turboacc luci-app-wol curl ca-certificates
+	ddns-scripts_aliyun ddns-scripts_dnspod luci-app-ddns luci-app-upnp luci-app-arpbind \
+  luci-app-filetransfer luci-app-vlmcsd luci-app-accesscontrol luci-app-turboacc curl ca-certificates mysettings
 
 ifneq ($(DUMP),)
   all: dumpinfo
